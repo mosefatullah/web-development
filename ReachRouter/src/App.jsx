6ba5d6d82fb65bdd30fe02425f535c9b5d0d8490
mosefatullah <mosefatullah@gmail.com>
2024-02-07 18:18:15 +0600

    .

--- ReachRouter/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// Reach Router v2 and React Router v6 are the same
-
-function App() {
- return <div>App</div>;
-}
-
-export default App;
--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+
+/*
+ * @NOTE:
+ * Reach Router v2 and React Router v6 are the same, so I will avoid using Reach Router.
+ */
 
 function App() {
-  return (
-    <div>App</div>
-  )
+ return <div>App</div>;
 }
 
-export default App
+export default App;
